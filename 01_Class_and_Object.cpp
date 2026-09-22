b72e9bfd5ea2c46d13bd759b7ca2e1dc2101169b
shravanihalake <shravanihalake243@gmail.com>
2026-09-22 19:56:07 +0530

    Create 01_Class_and_Object.cpp

---
+++ 01_Class_and_Object.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int age;
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main() {
+    Student s1;
+
+    s1.name = "Shravani";
+    s1.age = 18;
+
+    s1.display();
+
+    return 0;
+}
