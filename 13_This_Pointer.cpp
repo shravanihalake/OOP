3d43a303145137f57c00bcb62b1b8a215307d429
shravanihalake <shravanihalake243@gmail.com>
2026-09-22 20:18:05 +0530

    Create 13_This_Pointer.cpp

---
+++ 13_This_Pointer.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int age;
+
+public:
+    Student(string name, int age) {
+        this->name = name;
+        this->age = age;
+    }
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main() {
+    Student s("Shravani", 18);
+
+    s.display();
+
+    return 0;
+}
