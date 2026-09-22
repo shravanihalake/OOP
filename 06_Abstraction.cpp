735b5a568795262e02d122ca02074a8cc19d9444
shravanihalake <shravanihalake243@gmail.com>
2026-09-22 20:08:29 +0530

    Create 06_Abstraction.cpp

---
+++ 06_Abstraction.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() = 0;
+};
+
+class Dog : public Animal {
+public:
+    void sound() {
+        cout << "Dog barks" << endl;
+    }
+};
+
+int main() {
+    Dog d;
+
+    d.sound();
+
+    return 0;
+}
